14502419e8fd20b30d0f9fd1f59c0bda4fee5c20
ArkanZAG <142731011+ArkanZAG@users.noreply.github.com>
2024-06-29 16:12:11 +0700

    test

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Item
+    {
+        public Sprite itemSprite;
+        public string itemId;
+        public int amount;
+    }
+}
